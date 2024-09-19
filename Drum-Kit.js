00692a2514e38eda8a45e9ad3ccc12d443c5a012
lizz234 <111284844+lizz234@users.noreply.github.com>
2024-09-19 19:27:49 +0200

    Add footer

--- Drum-Kit.js
+++ Drum-Kit.js
@@ -1,25 +1,24 @@
-async function playSound (e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; //stop function from running together
-    audio.currentTime = 0; //rewind to the start
+async function playSound(e) {
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
     await sleep(1000)
     key.classList.remove('playing');
-  };
 
-  function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-  }
+    
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-
-  function removeTransition(e) {
+function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
+}
 
-  }
-
-  const keys = document.querySelectorAll('.keys');
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown' , playSound);
+const keys = document.querySelectorAll('.keys');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
